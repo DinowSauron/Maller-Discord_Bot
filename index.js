2adc4eb4c0aa7302f4153633233c29b9757169fd
Luiz Claudio <luiz.deadzone@gmail.com>
2020-12-05 17:46:06 -0300

    more test

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const Discord = require("discord.js");
 const fs = require('fs');
 const bot = new Discord.Client();
 
-
-/* Esconde isso */
 const token = process.env.token;
+
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+express()
+.use(express.static(path.join(__dirname, "public")))
+.set("views" path.join(__dirname, "views"))
+.set("view engine", "ejs")
+.get("/", (req, res) => res.render("pages/index"))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
 bot.login(token);
 bot.on("ready", () => {
@@ -18,6 +28,10 @@ bot.on("message", (msg) => {
     }
 })
 
+
+
+
+
 /* Instruções
 heroku login > loga na heroku
 git init > inicia o repositorio
@@ -25,4 +39,6 @@ heroku git: bot-app-name > inicia o bot na heroku
 git add . > envia os arquivos
 git commit -am "" > comita o codigo
 git push heroku main > envia o bot pra heroku
+
+heroku ps:restart web.1 >reinicia o servidor
 */
